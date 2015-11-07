e54b76c6379fc91597ab7a94a5691a726133026b
Ryan Huber <rhuber@gmail.com>
2015-11-07 03:32:29 +0000

    don't redial every time we send a message

--- syslog_sender.go
+++ syslog_sender.go
@@ -4,7 +4,11 @@ import (
 	"log/syslog"
 )
 
+var my_log *syslog.Writer
+
 func logLine(data string) {
-	my_log, _ := syslog.Dial("", "", syslog.LOG_INFO, "auditd")
+	if my_log == nil {
+		my_log, _ = syslog.Dial("", "", syslog.LOG_INFO, "auditd")
+	}
 	my_log.Write([]byte(data))
 }
