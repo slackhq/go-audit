a711c2f2a12d42a01b2f5d3da4756b3e8aa420fa
Ryan Huber <rhuber@gmail.com>
2015-11-08 01:30:41 +0000

    don't redial this for every packet...derp

--- canary.go
+++ canary.go
@@ -9,15 +9,15 @@ import (
 )
 
 func canaryGo(host string, port string) {
+	conn, err := net.Dial("udp", fmt.Sprintf("%s:%s", host, port))
+	if err != nil {
+		return
+	}
+	defer conn.Close()
 	for {
 		//	p := make([]byte, 2048)
-		conn, err := net.Dial("udp", fmt.Sprintf("%s:%s", host, port))
-		if err != nil {
-			return
-		}
 		fmt.Fprintf(conn, "P%dD", os.Getpid())
 		//	_, err = bufio.NewReader(conn).Read(p)
-		conn.Close()
 		time.Sleep(time.Second)
 	}
 }
