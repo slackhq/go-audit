03e5e48690e63db54fa1abaa6d57e55d84e4ffe4
Stephane Martin <stephane.martin@vesperal.eu>
2017-09-20 02:46:26 +0200

    sanity checks

--- parser.go
+++ parser.go
@@ -64,6 +64,7 @@ func NewAuditMessage(nlm *syscall.NetlinkMessage) *AuditMessage {
 
 // Gets the timestamp and audit sequence id from a netlink message
 func parseAuditHeader(msg *syscall.NetlinkMessage) (time string, seq int) {
+	var error err
 	headerStop := bytes.Index(msg.Data, headerEndChar)
 	// If the position the header appears to stop is less than the minimum length of a header, bail out
 	if headerStop < HEADER_MIN_LENGTH {
@@ -74,14 +75,24 @@ func parseAuditHeader(msg *syscall.NetlinkMessage) (time string, seq int) {
 	if header[:HEADER_START_POS] == "audit(" {
 		//TODO: out of range check, possibly fully binary?
 		sep := strings.IndexByte(header, headerSepChar)
+		if sep == -1 {
+			return
+		}
 		time = header[HEADER_START_POS:sep]
-		seq, _ = strconv.Atoi(header[sep+1:])
-
-		// Remove the header from data
-		msg.Data = msg.Data[headerStop+3:]
+		seq, err = strconv.Atoi(header[sep+1:])
+		if err != nil {
+			time = 0
+			seq = 0
+			return
+		}
+		if len(msg.Data) < (headerStop + 3) {
+			msg.Data = ""
+		} else {
+			// Remove the header from data
+			msg.Data = msg.Data[headerStop+3:]
+		}
 	}
-
-	return time, seq
+	return
 }
 
 // Add a new message to the current message group
